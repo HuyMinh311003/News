06622aa80d6b0c46ceaa3365972ff6865209cddf
Huy Minh <minh.th12558@sinhvien.hoasen.edu.vn>
2023-02-18 08:00:30 +0700

    news

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'News';
+
+  constructor (private authService: AuthService) {}
+
+  user!: User | null;
+  user$ = new Observable<User | null>;
+
+  ngOnInit(): void {
+    this.user$ = this.authService.user$;
+    this.user$.subscribe(user => {
+      this.user = user;
+      console.log(user);
+    })
+  }
 }
